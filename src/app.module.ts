40530175f1871a91912248d779e65e2399e2f276
Pedro Furlan <pedrofurlan6@gmail.com>
2023-11-19 17:11:45 -0300

    feat: add NestJs ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { envSchema } from './env'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountController],
-  providers: [PrismaService],
+  providers: [PrismaService, ConfigService],
 })
 export class AppModule {}
