9db3f6d20e3063eacd9dc39ab192d4a981e2d9a3
Pedro Furlan <pedrofurlan6@gmail.com>
2023-12-02 00:13:13 -0300

    test: add test to upload and create attachment use case

---
+++ test/storage/fake-uploader.ts
@@ -0,0 +1,24 @@
+import {
+  Uploader,
+  UploaderParams,
+} from '@/domain/forum/application/storage/uploader'
+import { randomUUID } from 'crypto'
+
+interface Upload {
+  fileName: string
+  url: string
+}
+
+export class FakeUploader implements Uploader {
+  public uploads: Upload[] = []
+
+  async upload({ fileName }: UploaderParams): Promise<{ url: string }> {
+    const url = randomUUID()
+    this.uploads.push({
+      fileName,
+      url,
+    })
+
+    return { url }
+  }
+}
