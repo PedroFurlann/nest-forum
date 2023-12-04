43d4437014fd44f4ee7873e1e3bbdad7e3f6bf37
Pedro Furlan <pedrofurlan6@gmail.com>
2023-12-03 22:52:27 -0300

    feat: add prisma comment with author mapper

---
+++ src/infra/database/prisma/mappers/prisma-comment-with-author-mapper.ts
@@ -0,0 +1,21 @@
+import { Comment as PrismaComment, User as PrismaUser } from '@prisma/client'
+
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+type PrismaCommentWithAuthor = PrismaComment & {
+  author: PrismaUser
+}
+
+export class PrismaCommentWithAuthorMapper {
+  static toDomain(raw: PrismaCommentWithAuthor): CommentWithAuthor {
+    return CommentWithAuthor.create({
+      commentId: new UniqueEntityID(raw.id),
+      authorId: new UniqueEntityID(raw.authorId),
+      authorName: raw.author.name,
+      content: raw.content,
+      createdAt: raw.createdAt,
+      updatedAt: raw.updatedAt,
+    })
+  }
+}
--- src/infra/database/prisma/repositories/prisma-question-comments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-question-comments-repository.ts
@@ -4,6 +4,8 @@ import { QuestionComment } from '@/domain/forum/enterprise/entities/question-com
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from '../prisma.service'
 import { PrismaQuestionCommentMapper } from '../mappers/prisma-question-comment-mapper'
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author'
+import { PrismaCommentWithAuthorMapper } from '../mappers/prisma-comment-with-author-mapper'
 
 @Injectable()
 export class PrismaQuestionCommentsRepository
@@ -61,5 +63,30 @@ export class PrismaQuestionCommentsRepository
     }
 
     return questionComments.map(PrismaQuestionCommentMapper.toDomain)
+  }
+
+  async findManyByQuestionIdWithAuthor(
+    questionId: string,
+    { page }: PaginationParams,
+  ): Promise<CommentWithAuthor[]> {
+    const questionComments = await this.prismaService.comment.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+      where: {
+        questionId,
+      },
+      include: {
+        author: true,
+      },
+    })
+
+    if (!questionComments) {
+      return [] as CommentWithAuthor[]
+    }
+
+    return questionComments.map(PrismaCommentWithAuthorMapper.toDomain)
   }
 }
